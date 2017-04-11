5af1590667051419f5784ef0acdcd16563c56cb6
VarIzo <varfalomei.izoldin@yandex.ru>
2017-04-11 12:38:25 +0300

    Подпаравил конфиг

--- upload/export/config.php
+++ upload/export/config.php
@@ -1,25 +1,27 @@
 <?php
+$rootDir  = dirname(__FILE__);
+$siteName = $_SERVER['HTTP_HOST'];
+
 // HTTP
-define('HTTP_SERVER',   'http://' . $_SERVER['HTTP_HOST'] . '/export/');
-define('HTTP_CATALOG', 'http://' . $_SERVER['HTTP_HOST']);
+define('HTTP_SERVER',   'http://' . $siteName . '/export/');
+define('HTTP_CATALOG', 'http://'  . $siteName);
 
 // HTTPS
-define('HTTPS_SERVER',  'https://' . $_SERVER['HTTP_HOST'] . '/export/');
-define('HTTPS_CATALOG', 'http://' . $_SERVER['HTTP_HOST']);
-
+define('HTTPS_SERVER',  'https://' . $siteName . '/export/');
+define('HTTPS_CATALOG', 'http://'  . $siteName);
 // DIR
-define('DIR_MODIFICATION',  dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/modification/');
-define('DIR_APPLICATION',   dirname(__FILE__) . '/subdomains/oc23/httpdocs/admin/');
-define('DIR_LANGUAGE',      dirname(__FILE__) . '/subdomains/oc23/httpdocs/export/language/');
-define('DIR_TEMPLATE',      dirname(__FILE__) . '/subdomains/oc23/httpdocs/export/view/template/');
-define('DIR_DOWNLOAD',      dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/download/');
-define('DIR_SYSTEM',        dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/');
-define('DIR_CATALOG',       dirname(__FILE__) . '/subdomains/oc23/httpdocs/catalog/');
-define('DIR_CONFIG',        dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/config/');
-define('DIR_UPLOAD',        dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/upload/');
-define('DIR_IMAGE',         dirname(__FILE__) . '/subdomains/oc23/httpdocs/image/');
-define('DIR_CACHE',         dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/cache/');
-define('DIR_LOGS',          dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/logs/');
+define('DIR_MODIFICATION',  $rootDir . '/subdomains/oc23/httpdocs/system/storage/modification/');
+define('DIR_APPLICATION',   $rootDir . '/subdomains/oc23/httpdocs/admin/');
+define('DIR_LANGUAGE',      $rootDir . '/subdomains/oc23/httpdocs/export/language/');
+define('DIR_TEMPLATE',      $rootDir . '/subdomains/oc23/httpdocs/export/view/template/');
+define('DIR_DOWNLOAD',      $rootDir . '/subdomains/oc23/httpdocs/system/storage/download/');
+define('DIR_SYSTEM',        $rootDir . '/subdomains/oc23/httpdocs/system/');
+define('DIR_CATALOG',       $rootDir . '/subdomains/oc23/httpdocs/catalog/');
+define('DIR_CONFIG',        $rootDir . '/subdomains/oc23/httpdocs/system/config/');
+define('DIR_UPLOAD',        $rootDir . '/subdomains/oc23/httpdocs/system/storage/upload/');
+define('DIR_IMAGE',         $rootDir . '/subdomains/oc23/httpdocs/image/');
+define('DIR_CACHE',         $rootDir . '/subdomains/oc23/httpdocs/system/storage/cache/');
+define('DIR_LOGS',          $rootDir . '/subdomains/oc23/httpdocs/system/storage/logs/');
 
 // DB заполняем из admin/config.php
 define('DB_HOSTNAME',   '');
