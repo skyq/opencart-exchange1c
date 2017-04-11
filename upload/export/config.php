a570b9c0cda7899fc87c45a70cb644be9063d6b7
VarIzo <varfalomei.izoldin@yandex.ru>
2017-04-11 12:37:41 +0300

    Добавил переменные в export/config.php

--- upload/export/config.php
+++ upload/export/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://oc23.skyq.ru/export/');
-define('HTTP_CATALOG', 'http://oc23.skyq.ru/');
+define('HTTP_SERVER',   'http://' . $_SERVER['HTTP_HOST'] . '/export/');
+define('HTTP_CATALOG', 'http://' . $_SERVER['HTTP_HOST']);
 
 // HTTPS
-define('HTTPS_SERVER', 'http://oc23.skyq.ru/export/');
-define('HTTPS_CATALOG', 'http://oc23.skyq.ru/');
+define('HTTPS_SERVER',  'https://' . $_SERVER['HTTP_HOST'] . '/export/');
+define('HTTPS_CATALOG', 'http://' . $_SERVER['HTTP_HOST']);
 
 // DIR
-define('DIR_APPLICATION', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/admin/');
-define('DIR_SYSTEM', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/system/');
-define('DIR_IMAGE', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/image/');
-define('DIR_LANGUAGE', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/export/language/');
-define('DIR_TEMPLATE', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/export/view/template/');
-define('DIR_CONFIG', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/system/config/');
-define('DIR_CACHE', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/system/storage/download/');
-define('DIR_LOGS', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/system/storage/modification/');
-define('DIR_UPLOAD', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/system/storage/upload/');
-define('DIR_CATALOG', '/home/httpd/vhosts/skyq.ru/subdomains/oc23/httpdocs/catalog/');
+define('DIR_MODIFICATION',  dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/modification/');
+define('DIR_APPLICATION',   dirname(__FILE__) . '/subdomains/oc23/httpdocs/admin/');
+define('DIR_LANGUAGE',      dirname(__FILE__) . '/subdomains/oc23/httpdocs/export/language/');
+define('DIR_TEMPLATE',      dirname(__FILE__) . '/subdomains/oc23/httpdocs/export/view/template/');
+define('DIR_DOWNLOAD',      dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/download/');
+define('DIR_SYSTEM',        dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/');
+define('DIR_CATALOG',       dirname(__FILE__) . '/subdomains/oc23/httpdocs/catalog/');
+define('DIR_CONFIG',        dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/config/');
+define('DIR_UPLOAD',        dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/upload/');
+define('DIR_IMAGE',         dirname(__FILE__) . '/subdomains/oc23/httpdocs/image/');
+define('DIR_CACHE',         dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/cache/');
+define('DIR_LOGS',          dirname(__FILE__) . '/subdomains/oc23/httpdocs/system/storage/logs/');
 
-// DB
-define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'a169819.mysql.mchost.ru');
-define('DB_USERNAME', 'a169819_bolt');
-define('DB_PASSWORD', 'VcKCVOtE3k');
-define('DB_DATABASE', 'a169819_bolt');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc23_');
+// DB заполняем из admin/config.php
+define('DB_HOSTNAME',   '');
+define('DB_USERNAME',   '');
+define('DB_PASSWORD',   '');
+define('DB_DATABASE',   '');
+define('DB_DRIVER',     '');
+define('DB_PREFIX',     '');
+define('DB_PORT',       '');
